23b013eeff7f2dd877b68bc27db5def56f0da934
ISenseToxicity <david@davidvanderwulp.nl>
2021-10-06 13:17:08 +0200

    testfx pompiewompie

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import javafx.scene.Parent;
+import org.loadui.testfx.GuiTest;
+
+public class SampleTest extends GuiTest {
+    @Override
+    protected Parent getRootNode() {
+        return null;
+    }
+}
