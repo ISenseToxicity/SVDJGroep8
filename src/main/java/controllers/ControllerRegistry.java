4017b04481a45bb752deac83f1be70c9612e8d71
Brandon <brrandonplokker@live.nl>
2021-10-06 13:42:59 +0200

    Controller Registry voor de juiste instance en interface aangemaakt.

--- src/main/java/controllers/ControllerRegistry.java
+++ src/main/java/controllers/ControllerRegistry.java
@@ -1,4 +1,25 @@
 package controllers;
 
+import java.util.Collection;
+import java.util.HashMap;
+
+// This class is meant to register all Controllers and only use one instance of each.
 public class ControllerRegistry {
+
+        protected  final static HashMap<String, Controller> controllers = new HashMap<>();
+
+        public static void register(Controller controller) {
+            String className = controller.getClass().getSimpleName();
+            controllers.put(className, controller);
+        }
+
+        public static Controller get(Class<? extends Controller> controllerClass) {
+            String className = controllerClass.getSimpleName();
+            return controllers.get(className);
+        }
+
+        public static Collection<Controller> getCollection() {
+            return controllers.values();
+        }
+
 }
