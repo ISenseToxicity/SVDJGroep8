f4a54d2adff9de7857b89f528d01637f7382927f
Brandon <brrandonplokker@live.nl>
2021-10-07 20:05:55 +0200

    Mijn mooie mooie animatie maar eens even toegevoegd. ApplicatieView is af.

--- src/main/java/controllers/ApplicatieController.java
+++ src/main/java/controllers/ApplicatieController.java
@@ -1,10 +1,18 @@
 package controllers;
 
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import views.ApplicatieView;
 
 public class ApplicatieController implements Controller {
@@ -12,6 +20,7 @@ public class ApplicatieController implements Controller {
     ApplicatieView applicatieView = new ApplicatieView();
 
     @FXML private Button beginButton;
+    @FXML private Label beginLabel;
 
     public void setStage(Stage primaryStage) {
         applicatieView.setStage(primaryStage);
@@ -19,6 +28,9 @@ public class ApplicatieController implements Controller {
 
     @FXML
     private void initialize() {
+
+        beginButton.setOnMouseEntered(e -> enterButton(beginButton, beginLabel));
+        beginButton.setOnMouseExited(e -> exitButton(beginButton, beginLabel));
 
         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
             @Override
@@ -32,4 +44,50 @@ public class ApplicatieController implements Controller {
         beginButton.setOnAction(event);
     }
 
+
+    private void background(Color bColor, Color tColor, Button button, javafx.scene.control.Label label) {
+        button.setBackground(new Background(new BackgroundFill(bColor, CornerRadii.EMPTY, Insets.EMPTY)));
+        button.setTextFill(tColor);
+        BorderWidths svdjBorder = new BorderWidths(2, 2, 0, 0);
+        Insets svdjInsets = new Insets(5, 0, 5, 10);
+        label.setBorder(new Border(new BorderStroke(
+                tColor,
+                BorderStrokeStyle.SOLID,
+                CornerRadii.EMPTY,
+                svdjBorder,
+                svdjInsets)));
+    }
+
+    private void enterButton(Button button, Label label) {
+        final Animation animation = new Transition() {
+            {
+                setCycleDuration(Duration.millis(500));
+                setInterpolator(Interpolator.EASE_OUT);
+            }
+            @Override
+            protected void interpolate(double frac) {
+                Color bColor = new Color(1, 1, 1, frac);
+                Color tColor = new Color(0, 0, 0, frac);
+                background(bColor, tColor, button, label);
+            }
+        };
+        animation.play();
+    }
+
+    private void exitButton(Button button, Label label) {
+        final Animation animation = new Transition() {
+            {
+                setCycleDuration(Duration.millis(500));
+                setInterpolator(Interpolator.EASE_OUT);
+            }
+            @Override
+            protected void interpolate(double frac) {
+                Color bColor = new Color(1, 1, 1, 1 - frac);
+                Color tColor = new Color(1, 1, 1, frac);
+                background(bColor, tColor, button, label);
+            }
+        };
+        animation.play();
+
+    }
 }
