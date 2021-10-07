1b62e4317af2ab818b479975c1bb7003f1873db2
Brandon <brrandonplokker@live.nl>
2021-10-07 19:12:29 +0200

    Begin knop verzet de scene nu naar de form.

--- src/main/java/controllers/ApplicatieController.java
+++ src/main/java/controllers/ApplicatieController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -9,8 +11,25 @@ public class ApplicatieController implements Controller {
 
     ApplicatieView applicatieView = new ApplicatieView();
 
+    @FXML private Button beginButton;
+
     public void setStage(Stage primaryStage) {
         applicatieView.setStage(primaryStage);
+    }
+
+    @FXML
+    private void initialize() {
+
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Stage primaryStage = applicatieView.getStage();
+                StartController startController = (StartController) ControllerRegistry.get(StartController.class);
+                startController.setStage(primaryStage);
+            }
+        };
+
+        beginButton.setOnAction(event);
     }
 
 }
