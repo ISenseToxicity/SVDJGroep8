c3444f00ef67806710cca18020632b1d3a398f21
ISenseToxicity <david@davidvanderwulp.nl>
2021-10-14 00:23:21 +0200

    added varianbles and getters and setters to Answers.java to make it compatible with the back-end

--- src/main/java/models/Answer.java
+++ src/main/java/models/Answer.java
@@ -1,4 +1,39 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Answer {
+    private String answerID;
+    private String answerText;
+    private ArrayList<String > categoryID;
+
+    public Answer(String answerID, String answerText, ArrayList<String> categoryID) {
+        this.answerID = answerID;
+        this.answerText = answerText;
+        this.categoryID = categoryID;
+    }
+
+    public String getAnswerID() {
+        return answerID;
+    }
+
+    public void setAnswerID(String answerID) {
+        this.answerID = answerID;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public ArrayList<String> getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(ArrayList<String> categoryID) {
+        this.categoryID = categoryID;
+    }
 }
