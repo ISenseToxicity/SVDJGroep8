2f87eefe7f3d168c4bdfd57dec57915e70313794
AntiEevee <eefjekarremans@hotmail.nl>
2021-10-15 14:13:02 +0200

    Route van  Request  gerealiseert En Instances geinstaleert

--- src/main/java/models/Request.java
+++ src/main/java/models/Request.java
@@ -1,4 +1,51 @@
 package models;
 
+import java.util.HashMap;
+
 public class Request {
+
+    //    Attributes
+    private final int userNr = 1;
+    private String duty;
+    private HashMap<String,String> givenVariables;
+    private boolean isNewData ;
+
+//    Constructor
+
+    public Request(String duty, HashMap<String,String> givenVariables, boolean isNewData) {
+        this.duty = duty;
+        this.givenVariables = givenVariables;
+        this.isNewData = isNewData;
+    }
+
+
+//    Setters And Gettes
+
+    public int getUserNr() {
+        return userNr;
+    }
+
+    public String getDuty() {
+        return duty;
+    }
+
+    public void setDuty(String duty) {
+        this.duty = duty;
+    }
+
+    public HashMap<String,String> getGivenVariables() {
+        return givenVariables;
+    }
+
+    public void setGivenVariables(HashMap<String,String> givenVariables) {
+        this.givenVariables = givenVariables;
+    }
+
+    public boolean isNewData() {
+        return isNewData;
+    }
+
+    public void setNewData(boolean newData) {
+        isNewData = newData;
+    }
 }
--- src/main/java/services/ConstructionService.java
+++ src/main/java/services/ConstructionService.java
@@ -1,4 +1,26 @@
 package services;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import models.Request;
+
+import java.util.HashMap;
+
 public class ConstructionService {
+
+    static ConstructionService constructionService;
+
+    public static ConstructionService getInstance(){
+        if (constructionService == null){
+            constructionService = new ConstructionService();
+        }
+        return constructionService;
+    }
+
+    public JsonElement constructJSON(Request request){
+
+
+        JsonElement jsonElement = new JsonArray();
+        return jsonElement;
+    }
 }
