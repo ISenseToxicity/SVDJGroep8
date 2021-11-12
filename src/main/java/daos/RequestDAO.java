77f3e49a63977d7f65d819faa50ccfb226a0543b
AntiEevee <eefjekarremans@hotmail.nl>
2021-11-12 14:30:38 +0100

    RemoevSpringboot unneeded refrence

--- src/main/java/daos/RequestDAO.java
+++ src/main/java/daos/RequestDAO.java
@@ -3,11 +3,6 @@ package daos;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.itextpdf.awt.geom.misc.HashCode;
-import models.Request;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import services.GetService;
 import services.ParameterStringBuilder;
 
