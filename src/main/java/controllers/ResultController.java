6e9a89f9ec5711f0a9cf1f0b9f371cdd497c4b05
MisterQuack <brandonplokker@live.nl>
2021-11-19 20:23:38 +0100

    Result MVC voorbereid

--- src/main/java/controllers/ResultController.java
+++ src/main/java/controllers/ResultController.java
@@ -1,4 +1,19 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import views.ResultView;
+
 public class ResultController implements Controller {
+    ResultView resultView = new ResultView();
+
+    public void setStage(Stage primaryStage) {
+        resultView.setStage(primaryStage);
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
 }
--- src/main/java/views/ResultView.java
+++ src/main/java/views/ResultView.java
@@ -1,6 +1,57 @@
 package views;
 
+import controllers.ControllerRegistry;
+import controllers.FormController;
+import controllers.ResultController;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-public class ResultView {
+import java.io.IOException;
+
+public class ResultView implements PrimaryStage {
+
+    private Stage primaryStage;
+
+    public ResultView() {
+
+    }
+
+    private void createPrimaryStage() throws IOException {
+        final int WIDTH = 1350;
+        final int HEIGHT = 750;
+        final String TITLE = "SVDJ - Subsidie Wijzer";
+        final String ICONURL = "fxml/icons/icon_SVDJ.png";
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ResultView.fxml"));
+        loader.setController(ControllerRegistry.get(ResultController.class));
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setMinWidth(WIDTH);
+        primaryStage.setMinHeight(HEIGHT);
+        primaryStage.setResizable(true);
+        primaryStage.setTitle(TITLE);
+        primaryStage.getIcons().add(new Image(ICONURL));
+
+        primaryStage.show();
+    }
+
+
+    @Override
+    public void setStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        try {
+            createPrimaryStage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getStage() {
+        return primaryStage;
+    }
+
 }
