59a109de30ac6e9b1b21dfc0463c291f140a0696
AntiEevee <eefjekarremans@hotmail.nl>
2021-11-19 02:17:50 +0100

    Request String to JSON
    Doesnt open cause QUESTIONS is empty

--- src/main/java/controllers/QuestionListController.java
+++ src/main/java/controllers/QuestionListController.java
@@ -1,16 +1,31 @@
 package controllers;
 
 
+import daos.QuestionListDAO;
 import models.Question;
 import models.QuestionList;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class QuestionListController implements Controller {
     private QuestionList questionList = new QuestionList();
 
     public void getQuestionListFromAPI(){
-        //todo get QuestionList from the DAO
+        QuestionListDAO questionListDAO = new QuestionListDAO();
+
+        try {
+            if(questionListDAO.getQuestionList() == 200){
+                //todo handle exeption
+            }
+
+        }
+        catch (IOException ioException){
+           ioException.getMessage();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
     public ArrayList<Question> getQuestions() {
         return questionList.getQuestions();
