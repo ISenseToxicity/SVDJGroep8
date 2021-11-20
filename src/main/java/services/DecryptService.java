cace9441e8fdbd5f238ede9bf79b43ab9cfafb4b
Kraylor <rob.heine@hotmail.com>
2021-11-20 21:52:52 +0100

    copy pasted services from backend and adjust audience etc. This is a wrong use for these tokens. not sure how to fix atm. -Rob-

--- src/main/java/services/DecryptService.java
+++ src/main/java/services/DecryptService.java
@@ -1,4 +1,21 @@
 package services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
 public class DecryptService {
+
+    public String decrypt(String token) {
+        byte[] key = new byte[64]; //my eyes are burning from this
+
+        Jws<Claims> result = Jwts.parserBuilder()
+                .requireAudience("Frontend")
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+
+        return result.getBody().get("Json", String.class);
+    }
+
 }
--- src/main/java/services/EncryptService.java
+++ src/main/java/services/EncryptService.java
@@ -1,4 +1,25 @@
 package services;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 public class EncryptService {
+
+    public String encrypt(String json) {
+        Instant now = Instant.now();
+        byte[] key = new byte[64];
+
+        return Jwts.builder()
+                .setSubject("Frontend")
+                .setAudience("Backend")
+                .claim("JsonString", json)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.MINUTES)))
+                .signWith(Keys.hmacShaKeyFor(key))
+                .compact();
+    }
 }
